bb8e85baf3e2407ba82391550fe641c1960df87e
emmawirts <emmaroselee14@gmail.com>
2025-03-06 13:05:47 -0500

    replace coronavirus with black squares

--- CoronavirusImmunityExtension/script.js
+++ CoronavirusImmunityExtension/script.js
@@ -1,2 +1,9 @@
-alert('Hello!');
-
+function replaceText(element) {
+    if (element.hasChildNodes()) {
+      element.childNodes.forEach(replaceText);
+    } else if (element.nodeType === Text.TEXT_NODE) {
+       element.textContent = element.textContent.replace(/coronavirus/gi, '■■■■■■■■■■■');
+    }
+  }
+  
+  replaceText(document.body);
